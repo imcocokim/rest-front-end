29e71b1279c4afa021f3b7f772e1238fd4822a90
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-07-25 12:15:42 -0700

    stub up css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,27 @@
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
+      <header className='App-Header'>
       {user ?
-        <nav>
+        <nav className={styles.navbar}>
           <ul>
             <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
             <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
             <li><Link to="/changePassword">Change Password</Link></li>
           </ul>
         </nav>
       :
-        <nav>
+        <nav className={styles.navbar}>
           <ul>
             <li><Link to="/login">Log In</Link></li>
             <li><Link to="/signup">Sign Up</Link></li>
           </ul>
         </nav>
       }
+      </header>
     </>
   )
 }
