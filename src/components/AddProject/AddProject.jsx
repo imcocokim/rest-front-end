7369e5c8cd855dec475fd1caeaa0128d184217d1
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-07-26 11:23:18 -0700

    add project stubbed up

--- src/components/AddProject/AddProject.jsx
+++ src/components/AddProject/AddProject.jsx
@@ -1,16 +1,50 @@
 import styles from './AddProject.module.css'
+import { useState, useRef, useEffect } from 'react';
 
 const AddProject = (props) => {
+
+  const [formData, setFormData] = useState({
+    title: '',
+    days: ''
+  })
+  const [validForm, setValidForm] = useState(false)
+  const formElement = useRef()
+
+  const handleChange = evt => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  }
+
+  useEffect(() => {
+		formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
+	}, [formData])
+
+  const handleSubmit= evt => {
+    evt.preventDefault()
+    props.handleAddProject(formData)
+  }
 
   return ( 
     <>
       <div className={styles.addProject}>
         <h3>Add Project</h3>
-        <form action="">
-          <label htmlFor="">Title</label>
-          <input type="text" />
-          <label htmlFor="">Number of Days</label>
-          <select name="days" id="days">
+        <form 
+          action=""
+          ref={formElement}
+          onSubmit={handleSubmit}
+          >
+          <label htmlFor="title-input">Title</label>
+          <input 
+            type="text"
+            value={formData.title}
+            onChange={handleChange}
+            />
+          <label htmlFor="days-input">Number of Days</label>
+          <select 
+            name="days" 
+            id="days" 
+            value={formData.days}
+            onChange={handleChange}
+            >
             <option value="">1</option>
             <option value="">2</option>
             <option value="">3</option>
@@ -19,7 +53,7 @@ const AddProject = (props) => {
             <option value="">6</option>
             <option value="">7</option>
           </select>
-          <button type="submit">Add Project</button>
+          <button type="submit" disabled={!validForm}>Add Project</button>
         </form>
       </div>
     </>
